2ad38f99e3e9f4c134b8c6ea1d80d96cf1fff810
anders.bjorkland <anders.bjorkland@kreationsbyran.se>
2023-01-17 14:01:08 +0100

    Add translation label and help messages for merchant

--- src/Form/Type/MerchantType.php
+++ src/Form/Type/MerchantType.php
@@ -18,20 +18,22 @@ class MerchantType extends \Symfony\Component\Form\AbstractType
     {
         $builder
             ->add('termsUrl', TextType::class, [
-                'label' => 'nca_sylius_klarna_gateway_plugin.ui.terms_url',
-                'required' => false,
+                'label' => 'nca_sylius_klarna_gateway_plugin.ui.terms_url.label',
+                'required' => true,
             ])
             ->add('checkoutUrl', TextType::class, [
-                'label' => 'nca_sylius_klarna_gateway_plugin.ui.checkout_url',
-                'required' => false,
+                'label' => 'nca_sylius_klarna_gateway_plugin.ui.checkout_url.label',
+                'required' => true,
             ])
             ->add('confirmationUrl', TextType::class, [
-                'label' => 'nca_sylius_klarna_gateway_plugin.ui.confirmation_url',
-                'required' => false,
+                'label' => 'nca_sylius_klarna_gateway_plugin.ui.confirmation_url.label',
+                'help' => 'nca_sylius_klarna_gateway_plugin.ui.confirmation_url.help',
+                'required' => true,
             ])
             ->add('pushUrl', TextType::class, [
-                'label' => 'nca_sylius_klarna_gateway_plugin.ui.push_url',
-                'required' => false,
+                'label' => 'nca_sylius_klarna_gateway_plugin.ui.push_url.label',
+                'help' => 'nca_sylius_klarna_gateway_plugin.ui.push_url.help',
+                'required' => true,
             ])
         ;
     }
