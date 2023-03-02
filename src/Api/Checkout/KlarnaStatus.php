d2f6846fe4a6b8b60b99fb5fc34391ab7e5c45ea
anders.bjorkland <anders.bjorkland@umain.com>
2023-03-02 13:38:06 +0100

    NCASyliusKlarnaGatewayPlugin-31 Update Klarna order if already created
    Redirect to confirmation page if status is paid

---
+++ src/Api/Checkout/KlarnaStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NorthCreationAgency\SyliusKlarnaGatewayPlugin\Api\Checkout;
+
+class KlarnaStatus
+{
+    public const CHECKOUT_COMPLETE = 'checkout_complete';
+}
